c18df2ef4ce4f475ac6942c6a991ecc160c33584
Tu Nombre <you@example.com>
2025-03-11 23:59:35 +0100

    Añadiendo la Carpeta PPS-Web

---
+++ web/script.js
@@ -0,0 +1,5 @@
+function changeMessage() {
+    const messageElement = document.querySelector('.message');
+    messageElement.innerHTML = '¡Gracias por hacer clic! 😊';
+    messageElement.style.color = '#00bcd4';
+}
